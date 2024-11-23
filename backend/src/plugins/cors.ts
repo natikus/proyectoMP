0a0624d925dece152796104fd032096274e327ee
natikus <natikumy2@gmail.com>
2024-11-23 14:33:53 -0300

    no funciona

--- backend/src/plugins/cors.ts
+++ backend/src/plugins/cors.ts
@@ -3,7 +3,7 @@ import cors, { FastifyCorsOptions } from "@fastify/cors";
 
 export default fp<FastifyCorsOptions>(async (fastify) => {
   fastify.register(cors, {
-    origin: ["https://localhost", "https://10.4.201.163"],
+    origin: ["https://localhost", "https://192.168.1.22"],
   });
   console.log("registre cors");
 });
